f7ec2cdc8d92d2aca660a6349e9634ec55280864
hcoeuret <henricoeuret@gmail.com>
2025-01-14 08:32:54 +0100

    More data structures

---
+++ complete-course-personal/09-Data-Structures-Operators/starter/challenge.js
@@ -0,0 +1,189 @@
+'use strict';
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+/* CHALLENGE 1 */
+/*
+let gk = "Neuer"
+const fieldPlayers = ["Davies1","Davies2","Davies3","Davies4","Davies5","Davies6","Davies7","Davies8","Davies9","Davies10",]
+
+const players1 = [gk, ...fieldPlayers];
+
+const players2 = [];
+
+const allplayers = [...players1, ...players2];
+
+const players1Final = [...players1, 'Thiago', 'Perisic','Coutinho'];
+
+const { odds : {team1, x : draw, team2}} = game;
+
+//let {team1, x : draw, team2} = game.odds;
+console.log(team1, draw, team2);
+
+const printGoals = function(...playerNames) {
+  for(let i = 0; i < playerNames.length; i++){
+    console.log(playerNames[i]);
+  }
+  console.log(`Number of goals ${playerNames.length}`)
+}
+
+printGoals(...game.players[0])
+
+console.log(team1 > team2 )
+
+team1 > team2 && console.log("team2 wins");
+team1 < team2 && console.log("team1 wins");
+*/
+
+
+
+/*
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+
+// 5.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6.
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+};
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+
+
+//1
+for(const [index, player] of game.scored.entries()){
+  console.log(`player ${player} scored goal number ${index +1 }`);
+}
+
+//2
+let cumul = 0 ;
+const oddsList = Object.values(game.odds)
+for( const odds of oddsList){
+  cumul += odds
+}
+console.log(cumul/oddsList.length)
+
+//3
+for(const [teamName, odds] of Object.entries(game.odds)){
+  console.log(`Odd of victory ${game[teamName] ?? 'draw'}: ${odds}`)
+};
+
+//bonus
+const scorers = {};
+
+const scorerTab = [];
+for(const players of game.scored){
+  let curr = scorers[players] ?? 0
+  scorers[players] = curr + 1;
+}
+console.log(scorers)
+
+*/
+
--- complete-course-personal/09-Data-Structures-Operators/starter/script.js
+++ complete-course-personal/09-Data-Structures-Operators/starter/script.js
@@ -30,6 +30,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -44,4 +48,180 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderDelivery: function({starterIndex = 1, mainIndex, time, address}){
+    console.log(starterIndex, time, address);
+  },
+
+  orderPasta : function(ing1, ing2, ing3){
+    console.log(`Here is your delicious pasta ${ing1}, ${ing2}, ${ing3}`);
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredient){
+    console.log(mainIngredient, otherIngredient);
+  }
 };
+
+
+
+const ordersSet = new Set(['pasta','pizza','risotto','pasta']);
+console.log(ordersSet)
+
+
+/*
+if(restaurant.openingHours.mon.open){
+  console.log(restaurant.openingHours.mon.open);
+}
+
+console.log(restaurant.openingHours.mon?.open);
+
+
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for(const item of menu){
+  console.log(item);
+}
+
+for(const [i, element] of menu.entries()){
+  console.log(`${i + 1}: ${element}`);
+}
+
+
+const properties = Object.keys(restaurant);
+console.log(properties);
+
+
+
+const rest1 = {
+  name : "Capri",
+  numGuests : 20,
+};
+
+const rest2 = {
+  name : "La piazza",
+  owner : 'Giorgio Armani',
+};
+
+rest2.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+
+let a = 1 ;
+
+a &&= 5;
+//console.log(a);
+
+//console.log(rest2.numGuests)
+
+//restaurant.orderPizza('mushrooms', 'tomato', 'mozzarela')
+
+console.log( 3 || 'Jonas');
+console.log("", 'Jonas'); //Jonas
+console.log(true || 0); //true
+console.log(undefined || null); //null
+
+restaurant.numGuests = 23;
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+
+const guests2 = restaurant.numGuests || 10
+console.log(guests1, guests2);
+
+console.log( 23 && 'Jonas' && '' && 'Hello' && null) //return undefined
+
+const ingre = ["test","test2","test3"];
+restaurant.orderPasta(...ingre)
+
+const newRestaurant = {...restaurant, founder : 'Giuseppe'}
+const newnewrestaurant = newRestaurant
+newRestaurant.name = "test";
+
+console.log(newRestaurant)
+console.log(newnewrestaurant)
+
+
+
+restaurant.orderDelivery({time : '22:30', address : "test", mainIndex: 2, strarterIndex :2});
+
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x,y,z] = arr;
+let [first, ,second] = restaurant.categories;
+console.log(first, second);
+[second, first] = [first, second];
+console.log(first, second)
+const [starter, main] = restaurant.order(2,0)
+console.log(restaurant.order(2,0))
+
+const nested = [2,4,[2,4]];
+const [f, d, [g,k]] = nested;
+console.log(f,d,g,k);
+
+const [p,q,r = 1] = [8,9];
+console.log(p,q,r);
+
+
+const {name, openingHours,categories} = restaurant;
+console.log(openingHours);
+const {name: restaurantName, openingHours : hours, categories : tags = [], menu = []} = restaurant;
+console.log(menu);
+
+let a = 111;
+let b = 999;
+const obj = {
+  a : 23,
+  b: 7,
+  c: 14
+};
+
+({a,b} = obj);
+console.log(a,b);
+
+
+
+const {openingHours : hours2} = restaurant
+
+console.log(fri)
+
+
+const arr = [7,8,9];
+const newArray = [1,2, ...arr];
+console.log(newArray);
+console.log(...newArray);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+console.log(newMenu);
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+const str = "Henri";
+const letters = [...str, ' ', 's'];
+console.log(letters)
+
+
+const [a, b, ...others] = [1,2,3,4,5];
+console.log(others);
+
+const [pizza, ,risotto, ...otherFood] = [
+  ...restaurant.mainMenu, ...restaurant.starterMenu];
+  console.log(pizza, risotto, otherFood);
+
+  const { sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays);
+
+const add = function(...numbers){
+  let sum = 0;
+  for(let i = 0; i< numbers.length; i++){
+    sum += numbers[i];
+  }
+  return sum;
+}
+
+console.log(add(5,3,7,2));
+*/
