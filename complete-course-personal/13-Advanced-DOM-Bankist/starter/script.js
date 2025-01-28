732faba18c227e4f229a57fa1e25855d1bc32187
hcoeuret <henricoeuret@gmail.com>
2025-01-28 20:15:31 +0100

    more section 13

---
+++ complete-course-personal/13-Advanced-DOM-Bankist/starter/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach((mod, i) => mod.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+
+//////////////////////////////////////////////////////////////////////
+///CODE
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e){
+  const s1coords = section1.getBoundingClientRect();
+  section1.scrollIntoView({behavior: 'smooth'});
+})
+  
+
+////////////////////////////////////////////////////////
+///////LECTURE
+
+const randomInt = (min,max) => Math.floor(Math.random() * (max-min+1) + min);
+
+const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+
+const highLev = document.querySelector('.nav__links');
+const lowLev = document.querySelector('.nav__link');
+const HighesLev = document.querySelector('.nav');
+
+highLev.addEventListener('click', function(){
+  this.style.backgroundColor = randomColor();
+});
+
+lowLev.addEventListener('click', function(){
+  this.style.backgroundColor = randomColor();
+});
+
+HighesLev.addEventListener('click', function(){
+  this.style.backgroundColor = randomColor();
+});
+
+
